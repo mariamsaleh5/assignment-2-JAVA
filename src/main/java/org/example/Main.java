4f28cdf45df04e281ab49e4ac238c90d1e6c2271
mariamsaleh <mariammidany11@gmail.com>
2026-07-15 21:37:42 +0300

    Initial commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,96 +6,157 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
+        String [] names= new String[5];
+        double [][] grades =new double[5][3];
+         for (int i=0;i<5;i++){
+             System.out.println("enter student "+(i+1));
+             names[i]=scanner.nextLine();
 
-        boolean correct = false;
-        int i = 1;
-        double balance = 2500.75;
+             for(int j=0;j<3;j++){
+                 boolean enter=false;
+                 do {
+                     System.out.println("enter subject " + (j + 1));
+
+                     double g = scanner.nextDouble();
+                     if (g >= 0 && g <= 100) {
+                         grades[i][j] = g;
+                         enter=true;
+                     } else {
+                         System.out.println("invalid grade");
+                     }
+                 }while (!enter);
+
+             }
+             scanner.nextLine();
+         }
+        int choose;
+         do{
+
+             System.out.println("Display the below menu:\n" +
+                     "1. Show All Students names.\n" +
+                     "2. Show all Students grades in each subject.\n" +
+                     "3. Search Student by name.\n" +
+                     "4. Count Passed Students\n" +
+                     "5. Display average for each subject\n" +
+                     "6. highest grade for each subject\n" +
+                     "7. Display letter grades (A-F).\n" +
+                     "0. Exit");
+             choose=scanner.nextInt();
+          switch (choose){
+              case 1:
+                  System.out.println("Show All Students names.");
+                  for (int i=0;i< names.length;i++){
+                      System.out.println(names[i]);
+                  }
+                  break;
+              case 2:
+                  for(int i=0;i< 5;i++){
+                      System.out.println("grade of student"+(i+1));
+                      for (int j=0;j< 3;j++){
+                          System.out.println("grade of student "+(i+1) +" of subject "+(j+1)+": is "+grades[i][j]);
+                      }
+                  }
+                  break;
+              case 3:
+                  scanner.nextLine();
+                  System.out.println("enter student name ");
+                 String name =scanner.nextLine();
+                 boolean found=false;
+                 for (int i=0;i< names.length;i++){
+                     if(names[i].equalsIgnoreCase(name)){
+                         found=true;
+                         System.out.println("student found in index "+ i);
+                         break;
+                     }
+
+                 }
+                 if(!found){
+                     System.out.println("user dont exit in system ");
+                 }
+                 break;
+              case 4:
+                  System.out.println("count pass students");
+                  int count=0;
+                  for(int i=0;i<5;i++){
+                      boolean passed=true;
+                      for(int j=0;j<3;j++){
+                          if(grades[i][j]<50){
+                              passed=false;
+                              break;
+                          }
+                      }
+                      if (passed){
+                          count++;
+                      }
 
 
-        do {
-            System.out.print("Enter 4-digit PIN: ");
-            String pin = scanner.nextLine();
+                  }
+                  System.out.println("passed students is "+count);
+                  break;
+              case 5:
+                  System.out.println("Display average for each subject");
+                  for(int j=0;j<3;j++){
+                      double sum=0;
+                      double avg=0;
 
-            if (pin.equals("1234")) {
-                correct = true;
+                      for (int i=0;i<5;i++){
+                          sum+=grades[i][j];
 
-                int choice = 0;
 
-                while (choice != 5) {
+                      }
+                      avg=sum/5;
+                      System.out.println("avg grade of subject"+(j+1)+": "+avg);
+                  }
+                  break;
+              case 6:
+                  System.out.println("highest grade  for each subject");
+                  for(int j=0;j<3;j++){
+                      double max=0;
+                      for (int i=0;i<5;i++){
+                          if(max<grades[i][j]){
+                              max=grades[i][j];
+                          }
+                      }
 
-                    System.out.println("\n========= ATM =========");
-                    System.out.println("1. Check Balance");
-                    System.out.println("2. Deposit");
-                    System.out.println("3. Withdraw");
-                    System.out.println("4. Show Account Status");
-                    System.out.println("5. Exit");
-                    System.out.println("=======================");
+                      System.out.println("max grade of subject"+(j+1)+": "+max);
+                  }
+                  break;
+              case 7:
+                  for(int i=0;i<5;i++){
+                      System.out.println("grades of student :"+(i+1));
+                      for(int j=0;j<3;j++){
+                          if(grades[i][j]>=85){
+                              System.out.println("A "+"Grade is :"+grades[i][j]);
+                          }
+                          else if(grades[i][j]>=75){
+                              System.out.println("B "+"Grade is :"+grades[i][j]);
+                          }
+                          else if(grades[i][j]>=65){
+                              System.out.println("C "+"Grade is :"+grades[i][j]);
+                          }
+                          else if(grades[i][j]>=50){
+                              System.out.println("D "+"Grade is :"+grades[i][j]);
+                          }else{
+                              System.out.println("F "+"Grade is :"+grades[i][j]);
+                          }
+                      }
+                  }
+              break;
 
-                    System.out.print("Enter your choice: ");
-                    choice = scanner.nextInt();
+              case 0:
+                  System.out.println("Exiting program...");
+                  break;
 
-                    switch (choice) {
+              default:
+                  System.out.println("Invalid choice.");
 
-                        case 1:
-                            System.out.println("Current Balance: " + balance);
-                            break;
+          }
 
-                        case 2:
-                            System.out.print("Enter deposit amount: ");
-                            double deposit = scanner.nextDouble();
 
-                            if (deposit > 0) {
-                                balance += deposit;
-                                System.out.println("Updated Balance: " + balance);
-                            } else {
-                                System.out.println("Invalid amount.");
-                            }
-                            break;
 
-                        case 3:
-                            System.out.print("Enter withdrawal amount: ");
-                            double withdraw = scanner.nextDouble();
-
-                            if (withdraw > 0 && withdraw <= balance) {
-                                balance -= withdraw;
-                                System.out.println("Updated Balance: " + balance);
-                            } else {
-                                System.out.println("Insufficient balance.");
-                            }
-                            break;
-
-                        case 4:
-                            if (balance >= 5000) {
-                                System.out.println("VIP Customer");
-                            } else if (balance >= 1000) {
-                                System.out.println("Regular Customer");
-                            } else {
-                                System.out.println("Low Balance");
-                            }
-                            break;
-
-                        case 5:
-                            System.out.println("Thank you for using our ATM.");
-                            break;
-
-                        default:
-                            System.out.println("Invalid option.");
-                    }
-
-                    scanner.nextLine();
-                }
-
-                break;
-            }
-
-            i++;
-
-        } while (i <= 3);
-
-        if (!correct) {
-            System.out.println("Your account has been locked.");
-        }
-
-        scanner.close();
+        }while(choose!=0);
+         scanner.close();
     }
+
+
 }
